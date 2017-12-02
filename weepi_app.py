9b4b209f7dc3c2e56987c6006369f69566dd0bf7
thebadger2017 <peterjtomlinson@outlook.com>
2017-12-02 17:51:31 +0000

    Added final details from tutorial

--- weepi_app.py
+++ weepi_app.py
@@ -6,9 +6,12 @@ from flask import jsonify
 from flask import abort
 from flask import make_response
 from flask import request
-
+from flask import url_for
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+
+auth = HTTPBasicAuth()
 
 tasks = [
     {
@@ -25,10 +28,29 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+@auth.get_password
+def get_password(username):
+    if username == 'norbert':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
-
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -38,6 +60,7 @@ def get_task(task_id):
     return jsonify({'task': task[0]})
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
 def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -51,6 +74,7 @@ def create_task():
     return jsonify({'task': task}), 201
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
@@ -69,6 +93,7 @@ def update_task(task_id):
     return jsonify({'task': task[0]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@auth.login_required
 def delete_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
